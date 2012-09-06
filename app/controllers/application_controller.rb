0857556846b74cb8492da7ebb07e94c4d33599da
jmeiss <jonathan@meiss.fr>
2012-09-06 16:29:24 +0200

    ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  before_filter :ensure_domain if Rails.env.production?
+
+  def ensure_domain
+    head :moved_permanently, :location => 'http://www.byod-manifesto.org'
+  end
+
 end
