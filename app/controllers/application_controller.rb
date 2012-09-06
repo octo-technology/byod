803606da9f183c6f5b557a4b80436bbc99a1ef7e
jmeiss <jonathan@meiss.fr>
2012-09-06 16:39:04 +0200

    ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :ensure_domain if Rails.env.production?
 
   def ensure_domain
-    domain = 'http://www.byod-manifesto.org/'
+    domain = 'www.byod-manifesto.org'
     raise request.host.inspect
     head :moved_permanently, :location => domain unless request.host == domain
   end
