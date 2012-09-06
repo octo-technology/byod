afecc0c517b79b35228ec5e1742f87e85b2e5b10
jmeiss <jonathan@meiss.fr>
2012-09-06 16:41:24 +0200

    ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   def ensure_domain
     domain = 'www.byod-manifesto.org'
-    raise request.host.inspect
     head :moved_permanently, :location => domain unless request.host == domain
   end
 
